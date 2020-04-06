047dcb607495821655011f543cafdd3b85e7c7e5
Emmanuel Egomson <femmor2u@gmail.com>
2020-04-06 16:04:56 -0500

    implemented the MeetPlayers and playerCard components

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Featured from "../Home/Featured/index";
 import MatchesHome from "./Matches/index";
+import MeetPlayers from "./MeetPlayers/index";
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
       <MatchesHome />
+      <MeetPlayers />
     </div>
   );
 };
