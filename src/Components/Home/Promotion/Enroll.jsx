fa27f6b7ff6e5a4b0555b09540661a2aa07ac4ab
Emmanuel Egomson <femmor2u@gmail.com>
2020-04-10 16:12:44 -0500

    finalized the enroll component and the homepage

--- src/Components/Home/Promotion/Enroll.jsx
+++ src/Components/Home/Promotion/Enroll.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Fade from "react-reveal/Fade";
 
+import { firebasePromotions } from "../../../firebase";
+
 import FormField from "../../ui/FormFields";
 
 import { validate } from "../../ui/misc";
@@ -28,6 +30,34 @@ class Enroll extends Component {
     },
   };
 
+  resetFormSuccess = (type) => {
+    const newFormData = { ...this.state.formData };
+
+    for (let key in newFormData) {
+      newFormData[key].value = "";
+      newFormData[key].valid = false;
+      newFormData[key].validationMessage = "";
+    }
+
+    this.setState({
+      formError: false,
+      formData: newFormData,
+      formSuccess: type
+        ? "Congratulations your email has been successfully submitted..."
+        : "Email is already exists in the database...",
+    });
+
+    this.clearSuccessMessage();
+  };
+
+  clearSuccessMessage() {
+    setTimeout(() => {
+      this.setState({
+        formSuccess: "",
+      });
+    }, 2000);
+  }
+
   submitForm = (e) => {
     e.preventDefault();
 
@@ -39,7 +69,18 @@ class Enroll extends Component {
       formIsValid = this.state.formData[key].valid && formIsValid;
     }
     if (formIsValid) {
-      console.log(dataToSubmit);
+      firebasePromotions
+        .orderByChild("email")
+        .equalTo(dataToSubmit.email)
+        .once("value")
+        .then((snapshot) => {
+          if (snapshot.val() === null) {
+            firebasePromotions.push(dataToSubmit);
+            this.resetFormSuccess(true);
+          } else {
+            this.resetFormSuccess(false);
+          }
+        });
     } else {
       this.setState({
         formError: true,
@@ -85,7 +126,12 @@ class Enroll extends Component {
                   Something is wrong, please try again!
                 </div>
               ) : null}
+              <div className="success_label">{this.state.formSuccess}</div>
               <button onClick={(e) => this.submitForm(e)}>Subscribe</button>
+              <div className="enroll_discl">
+                We promise not to share your email information without your
+                consent.
+              </div>
             </div>
           </form>
         </div>
