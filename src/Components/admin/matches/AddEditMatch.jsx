ab014fa0de01fc4e22268e6fb24a3bb4c6559a22
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-04 19:17:46 -0500

    completed the edit match functionality

--- src/Components/admin/matches/AddEditMatch.jsx
+++ src/Components/admin/matches/AddEditMatch.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AdminLayout from "../../../Hoc/AdminLayout"
 import FormField from "../../ui/FormFields"
+import { firebaseMatches, firebaseTeams, firebaseDB } from "../../../firebase"
+import { firebaseLooper } from "../../ui/misc"
 import { validate } from "../../ui/misc"
 
 class AddEditMatch extends Component {
@@ -12,7 +14,7 @@ class AddEditMatch extends Component {
         formSuccess: "",
         teams: [],
         formData: {
-            date: {
+              date: {
                 element: "input",
                 value: "",
                 config: {
@@ -166,10 +168,81 @@ class AddEditMatch extends Component {
         }
     }
 
+    updateForm = (element) => {
+        const newFormData = { ...this.state.formData };
+        const newElement = { ...newFormData[element.id] };
+        newElement.value = element.event.target.value;
+
+        let validData = validate(newElement);
+        newElement.valid = validData[0];
+        newElement.validationMessage = validData[1];
+
+        newFormData[element.id] = newElement;
+
+        this.setState({
+        formError: false,
+        formData: newFormData,
+        });
+    };
+
+    updateFields = (match, teamOptions, teams, type, matchId) => {
+        const newFormData = {
+            ...this.state.formData,
+        }
+
+        for(let key in newFormData) {
+            if(match){
+                newFormData[key].value = match[key]
+                newFormData[key].valid = true
+            }
+
+            if(key === "local" || key === "away") {
+                newFormData[key].config.options = teamOptions
+            }
+        }
+        
+        this.setState({
+            matchId,
+            formType: type,
+            formData: newFormData,
+            teams
+        })
+    }
+
     submitForm = (e) => {
         e.preventDefault();
     };
 
+    componentDidMount() {
+        const matchId = this.props.match.params.id
+        const getTeams = (match, type) => {
+            firebaseTeams.once("value")
+            .then(snapshot => {
+                const teams = firebaseLooper(snapshot)
+                const teamOptions = []
+
+                snapshot.forEach(childSnapshot => {
+                    teamOptions.push({
+                        key: childSnapshot.val().shortName,
+                        value: childSnapshot.val().shortName
+                    })
+                });
+                this.updateFields(match, teamOptions, teams, type, matchId)
+            })
+        }
+
+        if(!matchId) {
+            // Add Match
+        } else {
+            // Edit Match
+            firebaseDB.ref(`matches/${matchId}`).once("value")
+            .then((snapshot) => {
+                const match = snapshot.val()
+                getTeams(match, "Edit Match")
+            })
+        }
+    }
+    
 
     render() {
         return (
