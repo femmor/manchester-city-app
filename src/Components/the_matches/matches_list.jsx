c488fa8ed59de9832728163aebf0ac80240011a5
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-17 14:33:24 -0500

    created matches and matches list component

--- src/Components/the_matches/matches_list.jsx
+++ src/Components/the_matches/matches_list.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
 class MatchesList extends Component {
+
+    state = {
+        matchesList: []
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        return state = {
+            matchesList: props.matches
+        }
+    }
+
     render() {
+        console.log(this.state.matchesList)
         return (
             <div>
                 List
